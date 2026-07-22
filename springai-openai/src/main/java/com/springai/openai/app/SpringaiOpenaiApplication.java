c1edeed2261b75ca23bad1c3fca3ed8b768685e6
Marcos Magaña <imarcosmagana@gmail.com>
2026-07-21 19:34:42 -0600

    add project springia-openai

---
+++ springai-openai/src/main/java/com/springai/openai/app/SpringaiOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.springai.openai.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiOpenaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaiOpenaiApplication.class, args);
+    }
+
+}
